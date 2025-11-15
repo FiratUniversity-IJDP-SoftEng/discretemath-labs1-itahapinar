97cab41f700f3f0d215cddca0949add3b0eb738c
İbrahim Taha Pınar <154216157+itahapinar@users.noreply.github.com>
2025-11-15 12:12:43 +0300

    Update ex-6.py

--- ex-6.py
+++ ex-6.py
@@ -1,1 +1,22 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    if n % 2 == 0:
+        return n == 2
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+
+def pseudoprimes_base_2(limit=10000):
+    numbers = []
+    for n in range(2, limit+1):
+        if not is_prime(n): 
+            if pow(2, n-1, n) == 1:
+                numbers.append(n)
+    return numbers
+
+print(pseudoprimes_base_2())
 
