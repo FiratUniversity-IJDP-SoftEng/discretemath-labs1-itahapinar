6dac627d220189a034190ee48507e40145195996
İbrahim Taha Pınar <154216157+itahapinar@users.noreply.github.com>
2025-11-15 12:04:16 +0300

    Update ex-1.py

--- ex-1.py
+++ ex-1.py
@@ -1,1 +1,16 @@
+def is_prime(n):
+    if n < 2:
+        return False
+    if n % 2 == 0:
+        return n == 2
 
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+
+
+print(is_prime(17))  
+print(is_prime(20))  
