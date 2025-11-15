90bf779fc8170a20c091bfa2e267c38d6f7c82be
İbrahim Taha Pınar <154216157+itahapinar@users.noreply.github.com>
2025-11-15 12:05:15 +0300

    Update ex-2.py

--- ex-2.py
+++ ex-2.py
@@ -1,1 +1,21 @@
+def prime(n):
+    if n < 2:
+        return False
+    if n % 2 == 0:
+        return n == 2
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+
+def listofprimes(n):
+    primes = []
+    for k in range(2, n+1):
+        if prime(k):
+            primes.append(k)
+    return primes
+
+print(listofprimes(50))
 
